ec9fbae0fee6f3c7d56c585f0399ba51b65831d7
Chadjiminas Chrysostomos <cchadj01@cs.ucy.ac.cy>
2018-11-28 22:08:05 +0200

    Come on

---
+++ Assets/MatchColliderMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class MatchColliderMovement : MonoBehaviour {
+
+    public Transform tackedTransform;
+
+    private Rigidbody _rb;
+    private Collider _col;
+
+	// Use this for initialization
+	void Start () {
+        _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<Collider>();
+        _rb.isKinematic = false;
+        _rb.interpolation = RigidbodyInterpolation.Interpolate;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        _rb.MovePosition(tackedTransform.position);
+	}
+}
--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -2,16 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class MovingPlatform : MonoBehaviour {
 
-    
-	// Use this for initialization
+    public float downwardDisplacement;
+
+    private Rigidbody _rb;
+
+    private bool _moveDownwards = false;
+	// RigidBody must use interpolation and be kinematic
 	void Start () {
-		
+        _rb = GetComponent<Rigidbody>();
+        _rb.interpolation = RigidbodyInterpolation.Interpolate;
+        _rb.isKinematic = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private void Update()
+    {
+        if(_moveDownwards)
+        {
+            _rb.MovePosition(_rb.position + Vector3.down * downwardDisplacement * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _moveDownwards = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _moveDownwards = false;
+    }
 }
