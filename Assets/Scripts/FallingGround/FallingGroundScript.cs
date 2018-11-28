225db2089a5ea5e6edbf3434c86901e6a4f29dcb
Anastasios <ay222cn@lnu.se>
2018-11-28 19:14:41 +0200

    Working on New character

---
+++ Assets/Scripts/FallingGround/FallingGroundScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingGroundScript : MonoBehaviour {
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Fall();
+    }
+
+
+    private void Fall()
+    {
+        Debug.Log("STEPPING!");
+    }
+}
