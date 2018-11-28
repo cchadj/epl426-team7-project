798c29356a6417344e7f8d8c81d0589cc3baa4ba
Chadjiminas Chrysostomos <cchadj01@cs.ucy.ac.cy>
2018-11-28 23:17:03 +0200

    Please help me pleeeaaaasseeee.

---
+++ Assets/AttachPlayerOnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachPlayerOnTrigger : MonoBehaviour {
+
+    public List<Collider> collidersToAttach;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach(Collider c in collidersToAttach)
+        {
+            if(c == other )
+            {
+                other.transform.parent.parent = transform;
+                return;
+            }
+        }
+           
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+}
